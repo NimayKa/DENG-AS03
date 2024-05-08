daa312a7a3415ff31f313791a8ab6e1a8bd37f83
NimayKa <akyamin.pgibnu@gmail.com>
2024-05-08 21:32:36 +0800

    Update 1.6
    Rename Files
    Create new file for query only

---
+++ Assignment_DENG02_Query.sql
@@ -0,0 +1,30 @@
+--Query 1
+
+--Query 2
+
+--Query 3
+
+--Query 4
+
+--Query 5
+
+--Query 6
+
+--Query 7
+
+--Query 8
+
+--Query 9
+
+--Query 10
+
+--Query 11
+
+--Query 12
+
+--Query 13
+
+--Query 14
+
+--Query 15
+
